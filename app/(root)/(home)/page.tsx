2258cc61d8df46d3f754db002e376f55d93c9da2
Akbaraliyev_Mirsaid <157018820+Mirsaid95@users.noreply.github.com>
2025-02-25 00:16:39 +0500

    main

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+
+const Home = () => {
+
+  return (
+    <div className="max-w-[1240px] mx-auto py-2 px-2 flex items-center justify-between bg-orange-500">
+      <h1 className="text-[30px] font-bold">Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
